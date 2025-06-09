08b55599b2e740e0136bf621f706c511dd0893fd
kimih5186 <pookej2145@gmail.com>
2025-06-09 19:39:02 +0900

    상태창 추가

--- cat1/cat2.cpp
+++ cat1/cat2.cpp
@@ -26,8 +26,44 @@ int main(void) {
 
     Sleep(3000);
 
+    int intimacy = 2;
+    int mood = 3;
+    int cp = 0;
+    int soupCount = 0;
+    int hasToyMouse = 0, hasLaser = 0, hasScratcher = 0, hasTower = 0;
+    int catPos = HME_POS;
+    int prevPos = catPos;
+    int turn = 0;
 
+    while (1) {
+        turn++;
+        int roll;
+        int beforeMood = mood;
+        int beforeIntimacy = intimacy;
 
+        // 상태창
+        printf("==================== 현재상태 ====================\n");
+        printf("현재까지 만든 수프: %d개\n", soupCount);
+        printf("CP: %d 포인트\n", cp);
+        printf("쫀떡이 기분(0~3): %d\n", mood);
+        switch (mood) {
+        case 0: printf(" 기분이 매우 나쁩니다.\n"); break;
+        case 1: printf(" 심심해합니다.\n"); break;
+        case 2: printf(" 식빵을 굽습니다.\n"); break;
+        case 3: printf(" 골골송을 부릅니다.\n"); break;
+        }
+        printf("집사와의 관계(0~4): %d\n", intimacy);
+        switch (intimacy) {
+        case 0: printf(" 곁에 오는 것조차 싫어합니다.\n"); break;
+        case 1: printf(" 간식자판기 취급입니다.\n"); break;
+        case 2: printf(" 그럭저럭 쓸 만한 집사입니다.\n"); break;
+        case 3: printf(" 훌륭한 집사로 인정 받고 있습니다.\n"); break;
+        case 4: printf(" 집사 껌딱지입니다.\n"); break;
+        }
+        printf("============================================\n\n");
+        Sleep(3000);
+    }
 
 
 }
+
